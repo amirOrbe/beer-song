f33bbd9beba6a1ff98ed56eefb77ff52005d084b
Amir Orbe <orbebrian@gmail.com>
2021-03-22 16:01:52 -0600

    corregi errores en los saltos de linea y en el mapa

--- lib/beer_song.ex
+++ lib/beer_song.ex
@@ -5,19 +5,19 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(number) when number in 3..99 do
     # Your implementation here...
-    "#{number} bottles of beer on the wall, #{number} bottles of beer. \n Take one down and pass it around, #{number-1} bottles of beer on the wall. \n"
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.Take one down and pass it around, #{number-1} bottles of beer on the wall."
   end
 
   @spec verse(integer) :: String.t()
   def verse(number) when number == 2 do
     # Your implementation here...
-    "2 bottles of beer on the wall, 2 bottles of beer. \n Take one down and pass it around, 1 bottle of beer on the wall. \n"
+    "2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall."
   end
 
   @spec verse(integer) :: String.t()
   def verse(number) when number == 1 do
     # Your implementation here...
-    "1 bottle of beer on the wall, 1 bottle of beer. \nTake it down and pass it around, no more bottles of beer on the wall. \n"
+    "1 bottle of beer on the wall, 1 bottle of beer.Take it down and pass it around, no more bottles of beer on the wall."
   end
 
 
@@ -25,7 +25,7 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(number) when number == 0 do
     # Your implementation here...
-    "No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall. \n"
+    "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall."
   end
 
 
@@ -36,6 +36,5 @@ defmodule BeerSong do
   def lyrics(range) do
     # Your implementation here...
     Enum.map(range, &verse(&1))
-    lyrics(99..0)
   end
 end
