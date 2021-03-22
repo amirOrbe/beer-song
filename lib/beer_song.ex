35078e765f7054d84035e57660d7ef0d5dafe9cd
Amir Orbe <orbebrian@gmail.com>
2021-03-22 15:55:33 -0600

    se agrego saltos de linea

--- lib/beer_song.ex
+++ lib/beer_song.ex
@@ -5,22 +5,19 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(number) when number in 3..99 do
     # Your implementation here...
-    "#{number} botellas de cerveza en la pared, #{number} botellas de cerveza
-    Tome uno y paselo, #{number - 1} botellas de cerveza en la pared"
+    "#{number} bottles of beer on the wall, #{number} bottles of beer. \n Take one down and pass it around, #{number-1} bottles of beer on the wall. \n"
   end
 
   @spec verse(integer) :: String.t()
   def verse(number) when number == 2 do
     # Your implementation here...
-    "2 botellas de cerveza en la pared, 2 botellas de cerveza
-    Tome uno y paselo, 1 botella de cerveza en la pared"
+    "2 bottles of beer on the wall, 2 bottles of beer. \n Take one down and pass it around, 1 bottle of beer on the wall. \n"
   end
 
   @spec verse(integer) :: String.t()
   def verse(number) when number == 1 do
     # Your implementation here...
-    "1 botella de cerveza en la pared, 1 botella de cerveza
-    Tome uno y paselo, no mas botellas de cerveza en la pared"
+    "1 bottle of beer on the wall, 1 bottle of beer. \nTake it down and pass it around, no more bottles of beer on the wall. \n"
   end
 
 
@@ -28,8 +25,7 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(number) when number == 0 do
     # Your implementation here...
-    "No más botellas de cerveza en la pared, no más botellas de cerveza.
-     Ve a la tienda y compra más, 99 botellas de cerveza en la pared."
+    "No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more, 99 bottles of beer on the wall. \n"
   end
 
 
@@ -39,5 +35,7 @@ defmodule BeerSong do
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
     # Your implementation here...
+    Enum.map(range, &verse(&1))
+    lyrics(99..0)
   end
 end
