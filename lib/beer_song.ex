98bcfdd19c32894cd2b2108c60234ec701a09415
Amir Orbe <orbebrian@gmail.com>
2021-03-22 15:36:21 -0600

    se agrego el verso de la cancion

--- lib/beer_song.ex
+++ lib/beer_song.ex
@@ -3,9 +3,35 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) do
+  def verse(number) when number in 3..99 do
     # Your implementation here...
+    "#{number} botellas de cerveza en la pared, 99 botellas de cerveza
+    Tome uno y paselo, #{number - 1} botellas de cerveza en la pared"
   end
+
+  @spec verse(integer) :: String.t()
+  def verse(number) when number == 2 do
+    # Your implementation here...
+    "2 botellas de cerveza en la pared, 2 botellas de cerveza
+    Tome uno y paselo, 1 botella de cerveza en la pared"
+  end
+
+  @spec verse(integer) :: String.t()
+  def verse(number) when number == 1 do
+    # Your implementation here...
+    "1 botella de cerveza en la pared, 1 botella de cerveza
+    Tome uno y paselo, no mas botellas de cerveza en la pared"
+  end
+
+
+
+  @spec verse(integer) :: String.t()
+  def verse(number) when number == 0 do
+    # Your implementation here...
+    "No más botellas de cerveza en la pared, no más botellas de cerveza.
+     Ve a la tienda y compra más, 99 botellas de cerveza en la pared."
+  end
+
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
@@ -13,6 +39,5 @@ defmodule BeerSong do
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
     # Your implementation here...
-
   end
 end
